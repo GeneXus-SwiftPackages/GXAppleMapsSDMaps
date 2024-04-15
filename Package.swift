f6be6c5ca77df24d5101aa409c693c8c689b2b6d
genexusbot <builder@genexus.com>
2024-04-15 15:09:13 -0300

    Package version: 1.6.0-beta.4

--- Package.swift
+++ Package.swift
@@ -1,32 +1,32 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.7
 import PackageDescription
 
 let package = Package(
 	name: "GXAppleMapsSDMaps",
-	platforms: [.iOS("12.0"), .visionOS("1.0")],
+	platforms: [.iOS("12.0")],
 	products: [
 		.library(
 			name: "GXAppleMapsSDMaps",
 			targets: ["GXAppleMapsSDMapsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXAppleMaps.git", exact: "1.6.0-beta.3"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.6.0-beta.3"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXUCMaps.git", exact: "1.6.0-beta.3")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXAppleMaps.git", exact: "1.6.0-beta.4"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.6.0-beta.4"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXUCMaps.git", exact: "1.6.0-beta.4")
 	],
 	targets: [
 		.target(name: "GXAppleMapsSDMapsWrapper",
 				dependencies: [
 					"GXAppleMapsSDMaps",
-					.product(name: "GXAppleMaps", package: "GXAppleMaps", condition: .when(platforms: [.iOS, .visionOS])),
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .visionOS])),
-					.product(name: "GXUCMaps", package: "GXUCMaps", condition: .when(platforms: [.iOS, .visionOS]))
+					.product(name: "GXAppleMaps", package: "GXAppleMaps", condition: .when(platforms: [.iOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS])),
+					.product(name: "GXUCMaps", package: "GXUCMaps", condition: .when(platforms: [.iOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXAppleMapsSDMaps",
-			url: "https://pkgs.genexus.dev/iOS/beta/GXAppleMapsSDMaps-1.6.0-beta.3.xcframework.zip",
-			checksum: "3a011a082ef870147fa0f8decc2dfdda7d9ac9118f089184cb9f0429ccef5420"
+			url: "https://pkgs.genexus.dev/iOS/beta/GXAppleMapsSDMaps-1.6.0-beta.4.xcframework.zip",
+			checksum: "7d5b5da80e9ea77215b06b682136f554e46f75790a0902e5b3550486ffd7aa9b"
 		)
 	]
 )
