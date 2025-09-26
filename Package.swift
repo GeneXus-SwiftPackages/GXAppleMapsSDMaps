b9f3cd1d30067f1c3fb38ca283e502c76354ae78
The iOS Team @ GeneXus <builder@genexus.com>
2025-09-26 07:02:21 -0400

    Package version: 3.2.0

--- Package.swift
+++ Package.swift
@@ -1,32 +1,32 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
 	name: "GXAppleMapsSDMaps",
-	platforms: [.iOS("12.0")],
+	platforms: [.iOS("15.0"), .visionOS("2.0")],
 	products: [
 		.library(
 			name: "GXAppleMapsSDMaps",
 			targets: ["GXAppleMapsSDMapsWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXAppleMaps.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXUCMaps.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXAppleMaps.git", exact: "3.2.0"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "3.2.0"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXUCMaps.git", exact: "3.2.0")
 	],
 	targets: [
 		.target(name: "GXAppleMapsSDMapsWrapper",
 				dependencies: [
 					"GXAppleMapsSDMaps",
-					.product(name: "GXAppleMaps", package: "GXAppleMaps", condition: .when(platforms: [.iOS])),
-					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS])),
-					.product(name: "GXUCMaps", package: "GXUCMaps", condition: .when(platforms: [.iOS]))
+					.product(name: "GXAppleMaps", package: "GXAppleMaps", condition: .when(platforms: [.iOS, .visionOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .visionOS])),
+					.product(name: "GXUCMaps", package: "GXUCMaps", condition: .when(platforms: [.iOS, .visionOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXAppleMapsSDMaps",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXAppleMapsSDMaps-1.1.0.xcframework.zip",
-			checksum: "78fb208bc22b414a0353776a9648c3e82c212a00d8d372d21848986134920741"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXAppleMapsSDMaps-3.2.0.xcframework.zip",
+			checksum: "0f3f2cab993d0fc0462b11c857ebf9a4b18d54deadd70c41f6d529a94ba34cf5"
 		)
 	]
 )
